1a1980fd038a1fa82f3bca5048f82db26337a9c2
ljm1023 <west1023@163.com>
2019-04-07 15:53:28 +0800

    first commit

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  descriptions: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+    default: 10,
+  },
+  price: {
+    type: Number,
+    default: 0.00,
+  },
+  coverImg: {
+    type: String,
+  },
+  productCategory: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'ProductCategory'
+  }
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Product', productSchema);
